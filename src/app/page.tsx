85ac799706b6fb5dde81b02b0481556d65f88549
yamaryosan <canda3267@gmail.com>
2024-07-30 23:08:33 +0900

    [add] ノート一覧を表示（試運転）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,73 @@
 'use client';
 
 import { getDecks } from "@/lib/api/deckApi";
+import getNotes from "@/lib/api/noteApi";
 import { useEffect, useState } from "react";
+
+type note = {
+    noteId: string;
+    fields: {
+        表面: string;
+        裏面: string;
+    }
+}
 
 export default function Page() {
     const [decks, setDecks] = useState<string[]>([]);
+    const [selectedDeck, setSelectedDeck] = useState<string>('');
+    const [notes, setNotes] = useState<note[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
 
+    // デッキ一覧取得および初期デッキ設定
     useEffect(() => {
         async function fetchData() {
             const decks = await getDecks();
             setDecks(decks);
+            setSelectedDeck(decks[0]);
         }
         fetchData();
+        setLoading(false);
     }, []);
+
+    // デッキ変更時にノート取得
+    useEffect(() => {
+        async function fetchNotes() {
+            const notes = await getNotes(selectedDeck);
+            setNotes(notes);
+        }
+        fetchNotes();
+    }, [selectedDeck]);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+    
+    // デッキから選択
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedDeck(event.target.value);
+    }
 
     return (
         <div>
             <h1>テスト</h1>
-            <ul>
-                {decks.map(deck => <li key={deck}>{deck}</li>)}
-            </ul>
+            {/* デッキ選択 */}
+            <select name="decks" id="decks" onChange={handleChange}>
+                {decks.map((deck, index) => (
+                    <option key={index} value={deck}>{deck}</option>
+                ))}
+            </select>
+            <h2>ノート一覧</h2>
+            {notes.length === 0 ? (
+                <div>ノートがありません</div>
+            ) : (
+                <div>
+                    {notes.map((note) => (
+                        <div key={note.noteId}>
+                            <div>{note.noteId}</div>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
